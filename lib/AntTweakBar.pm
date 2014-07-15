ff50ea9a522183c717550f392a46e6e47df619b7
Ivan Baidakou <dmol@gmx.com>
2014-07-15 23:45:51 +0300

    Write documentation

--- lib/AntTweakBar.pm
+++ lib/AntTweakBar.pm
@@ -1,6 +1,8 @@
 package AntTweakBar;
 
 use 5.12.0;
+use strict;
+use warnings;
 
 use Carp;
 use Alien::AntTweakBar;
@@ -31,8 +33,139 @@ our @EXPORT = qw(
 
 our $VERSION = '0.01_02';
 
+=head1 NAME
+
+AntTweakBar - Perl bindings for AntTweakBar
+
+=head1 SYNOPSIS
+
+  use AntTweakBar qw/:all/;
+  use SDL::Events;
+
+  # Setup part: link AntTweakBar with your OpenGL/SDL system
+
+  AntTweakBar::init(TW_OPENGL);
+  AntTweakBar::window_size($width, $height);
+
+  # in your main rendering routine
+  sub display {
+    AntTweakBar::draw;
+  }
+
+  sub process_events {
+    SDL::Events::pump_events;
+    my $event = $self->sdl_event;
+    while (SDL::Events::poll_event($event)) {
+        ...;
+        AntTweakBar::eventSDL($event);
+    }
+  }
+
+
+  # define bars with variables
+  my $bar = AntTweakBar->new(
+    "TweakBar & Perl",
+    size  => '200 400',
+    color => '96 216 224'
+  );
+  my $enabled = 0;
+  $bar->add_variable(
+    mode       => 'rw',
+    name       => "Enabled",
+    type       => 'bool',
+    value      => \$enabled
+);
+
+
+=head1 DESCRIPTION
+
+=for HTML <p>
+<img src="http://anttweakbar.sourceforge.net/doc/data/media/tools/anttweakbar/twsimplesdl-128.jpg" style="max-width:100%;">
+</p>
+
+AntTweakBar (see L<http://anttweakbar.sourceforge.net/>) is nice tiny
+GUI library for OpenGL/SDL/DirectX applications.
+
+To display AntTweakBar in your OpenGL/SDL application you should do the
+following:
+
+=over
+
+=item intialize AntTweakBar(s):
+
+  AntTweakBar::init(TW_OPENGL);
+  AntTweakBar::window_size($width, $height);
+
+=item draw AntTweakBar(s):
+
+  AntTweakBar::draw;
+
+=item let AntTweakBar(s) respond to user interactions:
+
+  AntTweakBar::eventSDL($event);
+
+=item create AntTweakBar instance(s)
+
+  my $bar = AntTweakBar->new("TweakBar");
+
+=item add variables into the $bar
+
+  my $value = 3.14;
+  $bar->add_variable(
+    mode       => 'rw',
+    name       => "Enabled",
+    type       => 'integer',
+    value      => \$enabled
+  );
+
+=back
+
+=head2 EXPORT
+
+Constants only
+
+
+=head1 CONSTANTS
+
+The following constants let AntTweakBar know which graphic system do
+you use.
+
+=head2 TW_OPENGL
+
+=head2 TW_OPENGL_CORE
+
+=head2 TW_DIRECT3D9 (not implemented)
+
+=head2 TW_DIRECT3D10 (not implemented)
+
+=head2 TW_DIRECT3D11 (not implemented)
+
+=cut
+
 require XSLoader;
 XSLoader::load('AntTweakBar', $VERSION);
+
+
+=head1 METHODS
+
+=head2 new
+
+  my $bar = AntTweakBar->new(
+    "TweakBar & Perl",
+    size  => '200 400',
+    color => '96 216 224'
+  );
+
+  my $another_bar = AntTweakBar->new(
+    "Misc.",
+  );
+
+
+Creates new AntTweakBar instance. Optionally the list of strings of
+bar-related parameters can be provided. See the list of available
+at L<http://anttweakbar.sourceforge.net/doc/tools:anttweakbar:twbarparamsyntax>.
+
+=cut
 
 sub new {
     my ($class, $name, %bar_params) = @_;
@@ -52,6 +185,20 @@ sub DESTROY {
     _destroy($self->{_bar_ptr});
 }
 
+
+=head2 add_button
+
+  $bar->add_button(
+    name       => 'my_btn',
+    cb         => sub { say "clicked!" },
+    definition => "label='Click me!'",    # optional
+  );
+
+The definition parameters are the same as for variable. See
+L<http://anttweakbar.sourceforge.net/doc/tools:anttweakbar:varparamsyntax#parameters>.
+
+=cut
+
 sub add_button {
     my ($self, %args) = @_;
 
@@ -67,6 +214,13 @@ sub add_button {
     _add_button($self->{_bar_ptr}, $name, $cb, $definition);
 }
 
+
+=head2 add_separator
+
+  $bar->add_separator('separator-name');
+
+=cut
+
 sub add_separator {
     my ($self, $name, $definition) = @_;
     croak "Separator name should be specified"
@@ -76,6 +230,101 @@ sub add_separator {
 
     _add_separator($self->{_bar_ptr}, $name, $definition);
 }
+
+
+=head2 add_variable
+
+  my $zoom = 1.0;
+  $bar->add_variable(
+    mode       => 'rw',
+    name       => "Zoom",
+    type       => 'number',
+    value      => \$zoom,
+    definition => " min=0.01 max=2.5 step=0.01 help='Bla-bla-bla.' ",
+  );
+
+  my $bool = undef;
+  $bar->add_variable(
+    mode       => 'rw',
+    name       => "bool_rw_cb",
+    type       => 'bool',
+    cb_read    => sub { $bool; },
+    cb_write   => sub {
+        $bool = shift;
+        say "writing value $bool";
+    }
+  );
+
+C<mode>, C<name>, C<type> are mandatory. Either C<value> or C<cb_read>
+should be specified. The C<definition> and C<cb_write> are optional.
+
+=head3 mode
+
+The B<mode> can be C<rw> (read/write) or C<ro> (read only). The mode
+specified whether the variable value could be modified via AntTweakBar.
+
+=head3 name
+
+Defines the unique variable name at tweakbar. Unless C<label> is
+specified via C<defintion>, then C<name> also defines the visual label.
+for the variable.
+
+=head3 type
+
+Defines the type of variable. Original types L<http://anttweakbar.sourceforge.net/doc/tools:anttweakbar:twtype>
+has been reduced to:
+
+=over
+
+=item bool
+
+=item integer
+
+=item number (float)
+
+=item string
+
+=item color3f
+
+variable must be reference to array, consisted of 3 float values: rgb.
+
+=item color4f
+
+variable must be reference to array, consisted of 4 float values: rgba.
+
+=item direction
+
+3D-vector (direction). The variable must be reference to array, consisted of 3 float values.
+
+=item quaternion
+
+4D-vector (3D-object rotation). The variable must be reference to array, consisted of 4 float values.
+
+=item custom type of L<Anttweakbar::Type>
+
+=back
+
+=head3 value
+
+The B<reference> to the variable value. For complex types (e.g. quaternion) it must
+also be an B<reference> to array of 3 numbers.
+
+=head3 cb_read
+
+Closure, that returns the actual value of variable.
+
+=head3 cb_write($value)
+
+Closure, that is been invoked when user sets new value to the variable.
+If C<cb_write> is undefined, then the variable considered B<readonly>.
+
+=head3 definition
+
+An string that allows additiona tuning of variable.
+See L<http://anttweakbar.sourceforge.net/doc/tools:anttweakbar:varparamsyntax#parameters>
+for possible values.
+
+=cut
 
 sub add_variable {
     my ($self, %args) = @_;
@@ -105,15 +354,43 @@ sub add_variable {
                   $cb_read, $cb_write, $definition);
 }
 
+=head2 remove_variable($name)
+
+  $bar->remove_variable('Zoom');
+
+=cut
+
 sub remove_variable {
     my ($self, $name) = @_;
     _remove_variable($self->{_bar_ptr}, $name);
 }
 
+=head2 refresh
+
+  $bar->refresh;
+
+Tells Anttweakbar that variable values are possibly changed and
+should be updated.
+
+=cut
+
 sub refresh {
     my $self = shift;
     _refresh($self->{_bar_ptr});
 }
+
+=head2 set_bar_params(%values)
+
+  $bar->set_bar_params(
+    size        => '350 700,
+    valueswidth => '200'
+    visible     => 'false',
+  );
+
+Updates bar definition at runtime. See
+L<http://anttweakbar.sourceforge.net/doc/tools:anttweakbar:twbarparamsyntax>.
+
+=cut
 
 sub set_bar_params {
     my ($self, %params) = @_;
@@ -122,6 +399,15 @@ sub set_bar_params {
     }
 }
 
+=head2 set_variable_params($var_name, $var_definition)
+
+  $bar->set_variable_params('ObjRotation', readonly => 'true');
+
+Updates variable definition at runtime. See
+L<http://anttweakbar.sourceforge.net/doc/tools:anttweakbar:varparamsyntax#parameters>
+
+=cut
+
 sub set_variable_params {
     my ($self, $variable, %params) = @_;
     while (my ($k, $v) = each(%params)) {
@@ -129,57 +415,96 @@ sub set_variable_params {
     }
 }
 
-# Preloaded methods go here.
 
-1;
-__END__
-# Below is stub documentation for your module. You'd better edit it!
+=head1 INITIALIZATION AND DRAW FUNCTIONS
 
-=head1 NAME
 
-AntTweakBar - Perl extension for blah blah blah
+=head2 init
 
-=head1 SYNOPSIS
+  AntTweakBar::init(TW_OPENGL);
 
-  use AntTweakBar;
-  blah blah blah
+Initializes AntTweakBar
 
-=head1 DESCRIPTION
+=head2 terminate
 
-Stub documentation for AntTweakBar, created by h2xs. It looks like the
-author of the extension was negligent enough to leave the stub
-unedited.
+  AntTweakBar::terminate
 
-Blah blah blah.
+Uninitializes AntTweakBar
 
-=head2 EXPORT
+=head2 window_size
 
-None by default.
+  AntTweakBar::window_size(640, 480);
 
+Tell AntTweakBar the actual size of your window
+
+=head2 draw
+
+  AntTweakBar::draw;
+
+Draw AntTweakBar just before the frame buffer is presented (swapped).
+
+
+
+=head1 OPENGL EVENT FUNCTIONS
+
+
+=head2 eventMouseButtonGLUT
+
+  glutMouseFunc(\&AntTweakBar::eventMouseButtonGLUT);
+
+Let AntTweakBar handles mouse button clicks
+
+=head2 eventMouseMotionGLUT
+
+  glutMotionFunc(\&AntTweakBar::eventMouseMotionGLUT);
+  glutPassiveMotionFunc(\&AntTweakBar::eventMouseMotionGLUT);
+
+Let AntTweakBar handles mouse movements with pressed button(s)
+and passive mouse movements
+
+=head2 eventKeyboardGLUT
+
+  glutKeyboardFunc(\&AntTweakBar::eventKeyboardGLUT);
+
+Let AntTweakBar handles key presses
+
+=head2 eventSpecialGLUT
+
+  glutSpecialFunc(\&AntTweakBar::eventSpecialGLUT);
+
+=head2 GLUTModifiersFunc
+
+  AntTweakBar::GLUTModifiersFunc(\&glutGetModifiers);
+
+=head1 SDL EVENT FUNCTION
+
+=head2 eventSDL
+
+If you use SDL than it is more simple to let AntTweakBar process
+all input-related events via single call:
+
+  AntTweakBar::eventSDL($sdl_event);
 
 
 =head1 SEE ALSO
 
-Mention other useful documentation such as the documentation of
-related modules or operating system documentation (such as man pages
-in UNIX), or any relevant external documentation such as RFCs or
-standards.
 
-If you have a mailing list set up for your module, mention it here.
+L<Alien::AntTweakBar>, L<SDL>, L<OpenGL>, L<http://anttweakbar.sourceforge.net/>
 
-If you have a web site set up for your module, mention it here.
 
 =head1 AUTHOR
 
-A. U. Thor, E<lt>dmol@(none)E<gt>
+Ivan Baidakou E<lt>dmol@(gmx.com)E<gt>
+
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 2014 by A. U. Thor
+Copyright (C) 2014 by Ivan Baidakou
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself, either Perl version 5.20.0 or,
 at your option, any later version of Perl 5 you may have available.
 
-
 =cut
+
+1;
