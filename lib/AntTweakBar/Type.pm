90518d8561bb06bc0239807e8f05873c980b8f5a
Ivan Baidakou <dmol@gmx.com>
2014-07-18 11:51:42 +0300

    Add documentation for AntTweakBar::Type

--- lib/AntTweakBar/Type.pm
+++ lib/AntTweakBar/Type.pm
@@ -6,6 +6,42 @@ use Carp;
 use Alien::AntTweakBar;
 use AntTweakBar;
 
+
+=head1 NAME
+
+AntTweakBar::Type - User-defined variable types (enumerations) for AntTweakBar
+
+=head1 SYNOPSIS
+
+  my $framework_type = AntTweakBar::Type->new(
+    "framework_type",
+    [qw/Mojolicious Kelp Dancer Catalyst/],
+  );
+
+  my $fw_index = 1; # default will be Kelp
+  $bar->add_variable(
+    mode       => 'rw',
+    name       => "used_framework",
+    type       => $framework_type,
+    value      => \$fw_index,
+  );
+
+  my $gender_type = AntTweakBar::Type->new(
+    "gender_type",
+    { male => 1, female => 2},
+  );
+
+
+=head1 DESCRIPTION
+
+C<AntTweakBar::Type> allows to insert into bar variables of custom type.
+Currently only enumerations are supported.
+
+An perl variable must be refernce to integer; so, the type definition
+must be eithe array or hashref with integer values.
+
+=cut
+
 sub new {
     my ($class, $name, $value) = @_;
     my $hash = ref($value) eq 'ARRAY'
